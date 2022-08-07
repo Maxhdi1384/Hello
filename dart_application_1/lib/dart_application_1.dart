70c71e9ffe50a5f65e658665cb27caea7655c65e
Maxhdi <farahbakhsh1384fa@gmail.com>
2022-08-07 19:51:49 +0430

    first commit

---
+++ dart_application_1/lib/dart_application_1.dart
@@ -0,0 +1,11 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_application_1;
+
+export 'src/dart_application_1_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
+main(){
+print("Hello World");
+}
